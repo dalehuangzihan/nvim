2a3b1d7890d9599d962db0ff277e4a732bb163a1
Dale Huang <huangzihan98@gmail.com>
2024-04-05 13:47:19 +0100

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # nvim
 NeoVim Configurations
+
+## Branches:
+### `main`:
+    * No special colour schemes are installed. This is to retain compatibility with MacOS's default terminal, which can only display true colours. Using custom nvim colour themes might cause the colours to mess up. *NB:* This issue does not manifest if tmux is used inside default terminal.
--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "rust", "java", "python", "c", "lua", "vim", "vimdoc", "query" , "json", "xml", "yaml" },
+  ensure_installed = { "rust", "java", "python", "c", "lua", "vim", "vimdoc", "query" , "json", "xml", "yaml", "markdown" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
