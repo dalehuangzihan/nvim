aaebdb793853d68a035d1406e7c0143f92b92cb8
Dale Huang <huangzihan98@gmail.com>
2024-04-06 12:51:53 +0100

    set color scheme to onedarkpro

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,6 +1,35 @@
 function ColorMyPencils(color)
+	vim.cmd("autocmd VimEnter * hi Normal ctermbg=NONE guibg=NONE")
+	
 	-- set color scheme
-	color = color -- or "rose-pine"
+
+--	color = color or "gruvbox"
+--	vim.cmd("let g:gruvbox_transparent_bg = 1")
+	
+	color = color or "onedark"
+	require("onedarkpro").setup({
+		highlights = {
+			Comment = { italic = true },
+			Directory = { bold = true },
+			ErrorMsg = { italic = true, bold = true }
+		},
+		styles = {
+			types = "NONE",
+			methods = "NONE",
+			numbers = "NONE",
+			strings = "NONE",
+			comments = "italic",
+			keywords = "bold,italic",
+			constants = "NONE",
+			functions = "italic",
+			operators = "NONE",
+			variables = "NONE",
+			parameters = "NONE",
+			conditionals = "italic",
+			virtual_text = "NONE",
+		}
+	})
+
 	vim.cmd.colorscheme(color)
 
 	-- set background to be transparent
