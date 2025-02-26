d3ccae856c79192c4202a7521cc3467b3357690b
Dale Huang <huangzihan98@gmail.com>
2025-02-26 00:57:08 +0000

    set kanagawa colorscheme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -6,55 +6,55 @@ function ColorMyPencils(color)
 --	color = color or "gruvbox"
 --	vim.cmd("let g:gruvbox_transparent_bg = 1")
 	
-	color = color or "onedark"
-	require("onedarkpro").setup({
-		highlights = {
-			Comment = { italic = true },
-			Directory = { bold = true },
-			ErrorMsg = { italic = true, bold = true }
-		},
-		styles = {
-			types = "NONE",
-			methods = "NONE",
-			numbers = "NONE",
-			strings = "NONE",
-			comments = "italic",
-			keywords = "bold,italic",
-			constants = "NONE",
-			functions = "italic",
-			operators = "NONE",
-			variables = "NONE",
-			parameters = "NONE",
-			conditionals = "italic",
-			virtual_text = "NONE",
-		}
-	})
+--	color = color or "onedark"
+--	require("onedarkpro").setup({
+--		highlights = {
+--			Comment = { italic = true },
+--			Directory = { bold = true },
+--			ErrorMsg = { italic = true, bold = true }
+--		},
+--		styles = {
+--			types = "NONE",
+--			methods = "NONE",
+--			numbers = "NONE",
+--			strings = "NONE",
+--			comments = "italic",
+--			keywords = "bold,italic",
+--			constants = "NONE",
+--			functions = "italic",
+--			operators = "NONE",
+--			variables = "NONE",
+--			parameters = "NONE",
+--			conditionals = "italic",
+--			virtual_text = "NONE",
+--		}
+--	})
 
---    color = color or "kanagawa"
---    require('kanagawa').setup({
---        compile = false,             -- enable compiling the colorscheme
---        undercurl = true,            -- enable undercurls
---        commentStyle = { italic = true },
---        functionStyle = {},
---        keywordStyle = { italic = true},
---        statementStyle = { bold = true },
---        typeStyle = {},
---        transparent = false,         -- do not set background color
---        dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
---        terminalColors = true,       -- define vim.g.terminal_color_{0,17}
---        colors = {                   -- add/modify theme and palette colors
---            palette = {},
---            theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
---        },
---        overrides = function(colors) -- add/modify highlights
---            return {}
---        end,
---        theme = "wave",              -- Load "wave" theme when 'background' option is not set
---        background = {               -- map the value of 'background' option to a theme
---            dark = "dragon",           -- try "dragon" !
---            light = "lotus"
---        },
---    })
+    color = color or "kanagawa"
+    require('kanagawa').setup({
+        compile = false,             -- enable compiling the colorscheme
+        undercurl = true,            -- enable undercurls
+        commentStyle = { italic = true },
+        functionStyle = {},
+        keywordStyle = { italic = true},
+        statementStyle = { bold = true },
+        typeStyle = {},
+        transparent = false,         -- do not set background color
+        dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+        terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+        colors = {                   -- add/modify theme and palette colors
+            palette = {},
+            theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+        },
+        overrides = function(colors) -- add/modify highlights
+            return {}
+        end,
+        theme = "wave",              -- Load "wave" theme when 'background' option is not set
+        background = {               -- map the value of 'background' option to a theme
+            dark = "wave",           -- try "dragon" !
+            light = "lotus"
+        },
+    })
 
 	vim.cmd.colorscheme(color)
 
