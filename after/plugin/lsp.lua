eb1af2d62538fa5ef604dbe68d264fe859b28e53
Dale Huang <huangzihan98@gmail.com>
2025-02-27 22:52:19 +0000

    modify lsp mappings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -33,9 +33,36 @@ lsp.on_attach(function(client, bufnr)
 	vim.keymap.set('n', 'go', function() vim.lsp.buf.type_definition() end, opts)
 	vim.keymap.set('n', 'gr', function() vim.lsp.buf.references() end, opts)
 	vim.keymap.set('n', 'gs', function() vim.lsp.buf.signature_help() end, opts)
-	vim.keymap.set('n', '<F2>', function() vim.lsp.buf.rename() end, opts)
-	vim.keymap.set({'n', 'x'}, '<F3>', function() vim.lsp.buf.format({async = true}) end, opts)
-	vim.keymap.set('n', '<F4>', function() vim.lsp.buf.code_action() end, opts)
+	vim.keymap.set('n', 'gR', function() vim.lsp.buf.rename() end, opts)
+	vim.keymap.set({'n', 'x'}, 'gF', function() vim.lsp.buf.format({async = true}) end, opts)
+	vim.keymap.set('n', 'gC', function() vim.lsp.buf.code_action() end, opts)
+
+    -- -- TODO: is broken... needs fix
+    -- -- fancy rename (https://blog.viktomas.com/graph/neovim-lsp-rename-normal-mode-keymaps/)
+    -- vim.keymap.set("n", "<leader>r", function()
+    --     -- when rename opens the prompt, this autocommand will trigger
+    --     -- it will "press" CTRL-F to enter the command-line window `:h cmdwin`
+    --     -- in this window I can use normal mode keybindings
+    --     local cmdId
+    --     cmdId = vim.api.nvim_create_autocmd({ "CmdlineEnter" }, {
+    --         callback = function()
+    --             local key = vim.api.nvim_replace_termcodes("<C-f>", true, false, true)
+    --             vim.api.nvim_feedkeys(key, "c", false)
+    --             vim.api.nvim_feedkeys("0", "n", false)
+    --             -- autocmd was triggered and so we can remove the ID and return true to delete the autocmd
+    --             cmdId = nil
+    --             return true
+    --         end,
+    --     })
+    --     vim.lsp.buf.rename()
+    --     -- if LPS couldn't trigger rename on the symbol, clear the autocmd
+    --     vim.defer_fn(function()
+    --         -- the cmdId is not nil only if the LSP failed to rename
+    --         if cmdId then
+    --             vim.api.nvim_del_autocmd(cmdId)
+    --         end
+    --     end, 500)
+    -- end, bufoptsWithDesc("Rename symbol"))
 
 end)
 
