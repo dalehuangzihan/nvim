924936eef74449e006ab9b395ce8587cf8dd4cb6
Dale Huang <huangzihan98@gmail.com>
2025-03-28 17:46:14 +0000

    clean up config for lsp zero

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,40 +2,70 @@ local lsp = require('lsp-zero')
 
 lsp.preset("recommended")
 
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	['<CR>'] = cmp.mapping.confirm({ select = true });
-	['<C-Space>'] = cmp.mapping.complete(),
-})
-
 -- lsp.set_preferences({
 --	sign_icons = {}
 -- })
 
+-------------------------
+-- # Set up nvim-cmp # --
+-------------------------
+
+local cmp = require('cmp')
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local cmp_mappings = lsp.defaults.cmp_mappings({
+    -- Navigate between completion items
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({ select = false }),
+    -- Ctrl+Space to trigger completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+    -- Scroll up and down in the completion documentation
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+})
+cmp.setup({
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp_mappings,
+    snippet = {
+        expand = function(args)
+            vim.snippet.expand(args.body)
+        end,
+    },
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+    }),
+})
+
+---------------------------------
+-- # Set up custom behaviour # --
+---------------------------------
+
 lsp.on_attach(function(client, bufnr)
-	-- see :help lsp-zero-keybindings
-	-- to learn the available actions
-	--  lsp.default_keymaps({
-		--	  buffer = bufnr,
-		--	  preserve_mappings = false,
-	--  })
-	
-	local opts = {buffer = bufnr, remap = false}
-	-- these will be buffer-local keybindings
-	-- because they only work if you have an active language server
-	vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
-	vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set('n', 'gD', function() vim.lsp.buf.declaration() end, opts)
-	vim.keymap.set('n', 'gi', function() vim.lsp.buf.implementation() end, opts)
-	vim.keymap.set('n', 'go', function() vim.lsp.buf.type_definition() end, opts)
-	vim.keymap.set('n', 'gr', function() vim.lsp.buf.references() end, opts)
-	vim.keymap.set('n', 'gs', function() vim.lsp.buf.signature_help() end, opts)
-	vim.keymap.set('n', 'gR', function() vim.lsp.buf.rename() end, opts)
-	vim.keymap.set({'n', 'x'}, 'gF', function() vim.lsp.buf.format({async = true}) end, opts)
-	vim.keymap.set('n', 'gC', function() vim.lsp.buf.code_action() end, opts)
+    -- see :help lsp-zero-keybindings to learn the available actions
+    --  lsp.default_keymaps(
+    --	  buffer = bufnr,
+    --	  preserve_mappings = false,
+    --  })
+
+    local opts = { buffer = bufnr, remap = false }
+    -- these will be buffer-local keybindings because they only work if you have an active language server
+
+    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set('n', 'gD', function() vim.lsp.buf.declaration() end, opts)
+    vim.keymap.set('n', 'gi', function() vim.lsp.buf.implementation() end, opts)
+    vim.keymap.set('n', 'go', function() vim.lsp.buf.type_definition() end, opts)
+    vim.keymap.set('n', 'gr', function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set('n', 'gs', function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set('n', 'gR', function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set({ 'n', 'x' }, 'gF', function() vim.lsp.buf.format({ async = true }) end, opts)
+    vim.keymap.set('n', 'gC', function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set( "n", "gl", function() vim.diagnostic.open_float() end, opts)
 
     -- -- TODO: is broken... needs fix
     -- -- fancy rename (https://blog.viktomas.com/graph/neovim-lsp-rename-normal-mode-keymaps/)
@@ -63,29 +93,34 @@ lsp.on_attach(function(client, bufnr)
     --         end
     --     end, 500)
     -- end, bufoptsWithDesc("Rename symbol"))
-
 end)
 
--- here you can setup the language servers
+---------------------------------
+-- # Set Up Language Servers # --
+---------------------------------
+
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  -- Replace the language servers listed here
-  -- with the ones you want to install
-  ensure_installed = {
-	'clangd',
-	'cmake',
-	'jsonls',
-	'jdtls',
-	'ltex',
-	'marksman',
-	'pylsp',
-	'lemminx',
-	'yamlls',
-  },
-  handlers = {
-    lsp.default_setup,
-  }
+    -- Replace the language servers listed here
+    -- with the ones you want to install
+    ensure_installed = {
+        'clangd',
+        'cmake',
+        'jsonls',
+        'jdtls',
+        'ltex',
+        'marksman',
+        'pylsp',
+        'lemminx',
+        'yamlls',
+    },
+    handlers = {
+        lsp.default_setup,
+    }
 })
 
+-----------------------
+-- # Trigger Setup # --
+-----------------------
 
 lsp.setup()
--- lua/dale/packer.lua
+++ lua/dale/packer.lua
@@ -7,30 +7,31 @@ return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
 
+    -- LSP Stuff
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        requires = {
+            {'neovim/nvim-lspconfig'},
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'L3MON4D3/LuaSnip'},
+            --- Uncomment the two plugins below if you want to manage the language servers from neovim
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
+        }
+    }
+
+    -- Other plugins
+    use "tpope/vim-surround"
+    use "wesQ3/vim-windowswap"
+    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use('mbbill/undotree')
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.6',
         -- or                            , branch = '0.1.x',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
-
-    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-    use('mbbill/undotree')
-
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v3.x',
-        requires = {
-            --- Uncomment the two plugins below if you want to manage the language servers from neovim
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
-
-            {'neovim/nvim-lspconfig'},
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'L3MON4D3/LuaSnip'},
-        }
-    }
-
     use {
         "nvim-neo-tree/neo-tree.nvim",
         branch = "v3.x",
@@ -41,7 +42,6 @@ return require('packer').startup(function(use)
             -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
         }
     }
-
     use {
         'rmagatti/auto-session',
         config = function()
@@ -50,7 +50,6 @@ return require('packer').startup(function(use)
             }
         end
     }
-
     use {
         "windwp/nvim-autopairs",
         event = "InsertEnter",
@@ -59,11 +58,11 @@ return require('packer').startup(function(use)
         end
     }
 
-    use "tpope/vim-surround"
-    use "wesQ3/vim-windowswap"
 
+    -- Colour Themes
     use {'morhetz/gruvbox', as = "gruvbox"}
     use {'olimorris/onedarkpro.nvim', as = "onedark"}
     use {'rebelot/kanagawa.nvim', as = "kanagawa"}
+
 end)
 
