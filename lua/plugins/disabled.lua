e5035555e3bee138d15a6fded4335e328185730b
Dale Huang <huangzihan98@gmail.com>
2025-05-12 17:47:33 +0100

    disable leap.nvim and flash.nvim to clear 's' keymap override

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,4 +1,6 @@
 return {
   { "folke/noice.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
+  { "ggandor/leap.nvim", enabled =false },
+  { "folke/flash.nvim", enabled = false },
 }
